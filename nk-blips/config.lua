0d84becd0e1608385be07b0b09bdaea82e56a3cd
Nikez <stoyanovx3@gmail.com>
2021-01-13 16:20:25 +0200

    commit

--- nk-blips/config.lua
+++ nk-blips/config.lua
@@ -1,10 +1,10 @@
--- FROM GOSHETO FOR NIKI AND UNLISTED WITH LOVE
+-- blips , colors -https://docs.fivem.net/docs/game-references/blips/
 
 Config = {}
 
 Config.blips = {
 
-    {name="ПЕДЕРАС2", colour=1, typeid=1, x = 0, y = 0, z = 0},
-    {name="ПЕДЕРАС2", colour=1, typeid=1, x = 0, y = 0, z = 0},
+    {name="example", colour=1, typeid=1, x = 0, y = 0, z = 0},
+    {name="example", colour=1, typeid=1, x = 0, y = 0, z = 0},
 
 }
